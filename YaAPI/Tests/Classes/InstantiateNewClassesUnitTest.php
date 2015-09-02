28d7b924f3a2cd073004b5554e08f891a220dbcf
Sven Versteegen <redeye.vess@gmail.com>
2015-09-02 04:06:01 +0200

    tests

---
+++ YaAPI/Tests/Classes/InstantiateNewClassesUnitTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @name        MemberVarSpacingSniff
+ * @package     Yaapi\Coding-Standards
+ * @copyright   Copyright (C) 2015 http://joomworker.com - All rights reserved.
+ * @license     GNU LESSER GENERAL PUBLIC LICENSE Version 2.1 or later - http://www.gnu.org
+ */
+
+class YaAPI_Tests_Classes_InstantiateNewClassesUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {
+        return [
+                21 => 1,
+                27 => 1,
+               ];
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getWarningList()
+    {
+        return [];
+    }
+}
