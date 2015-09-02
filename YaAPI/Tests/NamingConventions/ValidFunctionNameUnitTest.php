c6adec77576eeb308228a16a6a450d3c12dc85bd
Sven Versteegen <redeye.vess@gmail.com>
2015-09-02 20:13:10 +0200

    more tests

---
+++ YaAPI/Tests/NamingConventions/ValidFunctionNameUnitTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @name        ValidFunctionNameUnitTest
+ * @package     Yaapi\Coding-Standards
+ * @copyright   Copyright (C) 2015 http://joomworker.com - All rights reserved.
+ * @license     GNU LESSER GENERAL PUBLIC LICENSE Version 2.1 or later - http://www.gnu.org
+ */
+
+/**
+ * ValidFunctionNameUnitTest
+ *
+ * @since   1.0
+ */
+class YaAPI_Tests_NamingConventions_ValidFunctionNameUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getErrorList()
+    {
+        return [
+                4  => 1,
+                9  => 1,
+                14 => 1,
+               ];
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array<int, int>
+     */
+    public function getWarningList()
+    {
+        return [];
+    }
+}
